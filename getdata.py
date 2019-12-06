197795746d128de5ffe8539ce38d7ff1fc7098ea
wtxhpx1991 <wtxhpx1991@126.com>
2019-12-06 09:53:19 +0800

    get w.wsd data

--- getdata.py
+++ getdata.py
@@ -21,13 +21,11 @@ def get_wsd(*args):
     if len(code.split(',')) == 1:
         raw_data_result.columns = raw_data.Fields
         raw_data_result['wind_code'] = code
-        # raw_data_result.index.name = 'index'
         raw_data_result = raw_data_result.reset_index()
     if len(field.split(',')) == 1:
         raw_data_result.columns = raw_data.Codes
         raw_data_result = pd.melt(raw_data_result.reset_index(), id_vars='index', var_name='wind_code',
                                   value_name=raw_data.Fields[0])
-        # raw_data_result = raw_data_result.set_index(['index'])
     raw_data_result.rename(columns={'index': 'date'}, inplace=True)
     return raw_data_result
 
